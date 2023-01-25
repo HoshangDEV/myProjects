4f5d9330cde9f824711316bdc1f28cb468f3acd2
HoshangPC <79860218+HoshangPC@users.noreply.github.com>
2023-01-25 20:42:38 +0300

    less code

--- app.js
+++ app.js
@@ -4,33 +4,14 @@ let gs = document.querySelector('.guest-score');
 let h = 0
 let g = 0
 
-function ish1() {
-    h += 1
+function ish(num) {
+    h += num
     hs.innerHTML = h
 }
 
-function ish2() {
-    h += 2
-    hs.innerHTML = h
-}
-
-function ish3() {
-    h += 3
-    hs.innerHTML = h
-}
 // --------------------
 
-function isg1() {
-    g += 1
-    gs.innerHTML = g
-}
-
-function isg2() {
-    g += 2
-    gs.innerHTML = g
-}
-
-function isg3() {
-    g += 3
+function isg(num) {
+    g += num
     gs.innerHTML = g
 }
