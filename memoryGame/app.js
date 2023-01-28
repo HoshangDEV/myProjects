9250c9d7f4bf3421177acbbf7853b0b66c125bf5
HoshangPC <79860218+HoshangPC@users.noreply.github.com>
2023-01-28 21:21:22 +0300

    issue fixed

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -2,22 +2,20 @@ const btn = document.querySelector('button')
 const con = document.getElementById('con')
 const h2 = document.querySelector('h2')
 const startBtn = document.getElementById('startBtn')
-
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const randomNumbers = [];
 
 
 
 function start() {
+    h2.innerHTML = "😉 دەست بکە بەیاری"
 
     //generate random number
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const randomNumbers = [];
     while (numbers.length > 0) {
         const randomIndex = Math.floor(Math.random() * numbers.length);
         const randomNumber = numbers.splice(randomIndex, 1)[0];
         randomNumbers.push(randomNumber);
     }
-
-    h2.innerHTML = 'Lets Go 😉'
 
     //apply value to boxs
     let tempBtn = ""
@@ -27,47 +25,54 @@ function start() {
     con.innerHTML = tempBtn
     disableBtn(true)
 
-
     //hide after 1.5s
     setTimeout(function () {
         const h1Elements = document.getElementsByTagName("h1");
         for (let i = 0; i < h1Elements.length; i++) {
             h1Elements[i].style.visibility = "hidden";
-            disableBtn(false)
         }
-    }, 1500);
-
+        disableBtn(false)
+    }, 2000);
 }
 
 //start the game when page loaded
-window.onload = function () {
-    start()
-}
+// window.onload = function () {
+//     start()
+// }
 
 
 //show with click
 let temp = 1
+let timeOfLose = 0
 
 function btnPress(n) {
     let tempH = document.getElementById(`h${n}`)
     tempH.style.visibility = 'visible'
     if (temp === n) {
-        if (temp > 0 && temp >= 3) {
-            h2.innerHTML = 'Good 😃'
-        } else if (temp > 3 && temp >= 6) {
-            h2.innerHTML = 'WOW 😎'
-        } else if (temp > 6 && temp >= 8) {
-            h2.innerHTML = 'Almost There 🤯'
+        if (temp === 9) {
+            h2.innerHTML = '🙂 نانا لێێ دەزانی'
+        } else if (temp > 6 && temp <= 8) {
+            h2.innerHTML = '😁 بدۆڕێی دەعوەتم'
+        } else if (temp > 3 && temp <= 6) {
+            h2.innerHTML = '🙂 بە هیوای دۆڕان'
+        } else if (temp > 0 && temp <= 3) {
+            h2.innerHTML = '😃 ئاگام لێتە شێرەکەم'
         }
         temp += 1
-    } else if (temp === 10) {
-        h2.innerHTML = 'YOU WON!! 👏'
-    } else if(temp > n){
-
-    }else {
-        h2.innerHTML = 'You lose 😓'
+    } else {
+        if (timeOfLose === 0) {
+            h2.innerHTML = '😂 ئەک داوەشێی'
+        } else if (timeOfLose === 1) {
+            h2.innerHTML = '😂 حەیامان چووو'
+        } else if (timeOfLose === 2) {
+            h2.innerHTML = '😂 بمری باشترە'
+        }
         disableBtn(true)
         temp = 1
+        timeOfLose += 1
+        if(timeOfLose===2){
+            timeOfLose=0
+        }
     }
 }
 
