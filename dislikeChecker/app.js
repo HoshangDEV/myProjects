885d354dc9fb8ff54af5c8b48ef31b330ccc3db8
HoshangPC <79860218+HoshangPC@users.noreply.github.com>
2023-07-06 16:05:51 +0300

    Update formatDate app.js

--- dislikeChecker/app.js
+++ dislikeChecker/app.js
@@ -66,7 +66,15 @@ function formatNumber(number) {
 }
 
 function formatDate(date) {
-  const options = { year: "numeric", month: "long", day: "numeric" };
+  const options = { 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false
+  };
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
