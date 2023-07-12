7112f2f8252f6c9a45848b0bf87a81ac794e60f9
HoshangPC <79860218+HoshangPC@users.noreply.github.com>
2023-07-12 14:00:28 +0300

    updated

--- removeExtraSpace/app.js
+++ removeExtraSpace/app.js
@@ -10,23 +10,53 @@ function removeLines() {
   document.getElementById("outputTextArea").value = result;
 }
 
+function highlightWord() {
+  var paragraph = document.getElementById("inputTextArea").value;
+  var word = document.getElementById("find").value;
+  var highlightedParagraph = document.getElementById("outputTextArea");
+  var countElement = document.getElementById("count");
+
+  var regex = new RegExp(word, "gi");
+  var highlightedText = paragraph.replace(
+    regex,
+    '<span class="highlight">$&</span>'
+  );
+
+  highlightedParagraph.innerHTML = highlightedText;
+
+  var count = (highlightedText.match(regex) || []).length;
+  countElement.textContent = "Found " + count + " occurrence(s).";
+}
+
 function replaceWord() {
   const findWord = document.getElementById("find").value;
   const replaceWith = document.getElementById("replaceWith").value;
   var input = document.getElementById("inputTextArea").value;
-  var result = input.replaceAll(findWord, replaceWith).trim();
-  document.getElementById("outputTextArea").value = result;
 
-  // console.log(findWord)
+  // Create a regular expression with the 'gi' flags for case-insensitive and global matching
+  var regex = new RegExp(findWord, "gi");
+
+  // Use the regular expression to replace all occurrences of the word and apply highlighting
+  var result = input
+    .replace(regex, `<span class="highlight">${replaceWith}</span>`)
+    .trim();
+
+  document.getElementById("outputTextArea").innerHTML = result;
 }
+
+
+
 
 function copyToClipboard() {
   var output = document.getElementById("outputTextArea");
-  output.select();
-  output.setSelectionRange(0, 99999); // For mobile devices
+  var range = document.createRange();
+  range.selectNode(output);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
   document.execCommand("copy");
+
   var copyButton = document.getElementById("copyButton");
-  copyButton.innerText = "Coped!";
+  copyButton.innerText = "Copied!";
   setTimeout(function () {
     copyButton.innerText = "Copy Result";
   }, 2000);
