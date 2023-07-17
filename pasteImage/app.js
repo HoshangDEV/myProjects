fa526fec4faeadb8c2b2b198ebd624273621c441
HoshangPC <79860218+HoshangPC@users.noreply.github.com>
2023-07-17 16:18:53 +0300

    updated pasteImage

--- pasteImage/app.js
+++ pasteImage/app.js
@@ -27,11 +27,42 @@ function pasteImage() {
         data.forEach(function (item) {
           if (
             item.types.includes("image/png") ||
-            item.types.includes("image/jpeg")
+            item.types.includes("image/jpeg") ||
+            item.types.includes("image/gif") ||
+            item.types.includes("image/bmp") ||
+            item.types.includes("image/tiff") ||
+            item.types.includes("image/svg+xml") ||
+            item.types.includes("image/webp") ||
+            item.types.includes("image/x-icon") ||
+            item.types.includes("image/heic") ||
+            item.types.includes("image/heif") ||
+            item.types.includes("image/raw")
           ) {
             item
               .getType(
-                item.types.includes("image/png") ? "image/png" : "image/jpeg"
+                item.types.includes("image/png")
+                  ? "image/png"
+                  : item.types.includes("image/jpeg")
+                  ? "image/jpeg"
+                  : item.types.includes("image/gif")
+                  ? "image/gif"
+                  : item.types.includes("image/bmp")
+                  ? "image/bmp"
+                  : item.types.includes("image/tiff")
+                  ? "image/tiff"
+                  : item.types.includes("image/svg+xml")
+                  ? "image/svg+xml"
+                  : item.types.includes("image/webp")
+                  ? "image/webp"
+                  : item.types.includes("image/x-icon")
+                  ? "image/x-icon"
+                  : item.types.includes("image/heic")
+                  ? "image/heic"
+                  : item.types.includes("image/heif")
+                  ? "image/heif"
+                  : item.types.includes("image/raw")
+                  ? "image/raw"
+                  : ""
               )
               .then(function (blob) {
                 handleImage(blob);
@@ -47,7 +78,6 @@ function pasteImage() {
       console.error("Failed to read clipboard data: ", error);
     });
 }
-
 // Handle the image
 function handleImage(blob) {
   const img = new Image();
@@ -59,22 +89,23 @@ function handleImage(blob) {
     // Create a button for each image
     const btn = document.createElement("button");
     btn.classList.add("downloadBtn");
-    btn.innerHTML = '<i class="fa-solid fa-circle-down"></i> Download Image';
+    btn.innerHTML =
+      '<i class="fa-solid fa-circle-down"></i> Download Image';
     btn.onclick = function () {
-      downloadImage(img.src, "image.jpg");
+      downloadImage(img.src, "image.png"); // Download as PNG
     };
 
     // Append the image and button to the span
     span.appendChild(img);
     span.appendChild(btn);
 
-if (dropzone.firstChild) {
-  // If there are child elements, insert the new element before the first child
-  dropzone.insertBefore(span, dropzone.firstChild);
-} else {
-  // If there are no child elements, append the new element as the first child
-  dropzone.appendChild(span);
-}
+    if (dropzone.firstChild) {
+      // If there are child elements, insert the new element before the first child
+      dropzone.insertBefore(span, dropzone.firstChild);
+    } else {
+      // If there are no child elements, append the new element as the first child
+      dropzone.appendChild(span);
+    }
   };
 
   const reader = new FileReader();
